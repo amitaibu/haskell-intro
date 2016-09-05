525f304eca2092c98c0e2b4f5953742f5bb34260
Amitai Burstein <amitai@gizra.com>
2016-09-05 20:07:38 +0300

    Sum monoid

--- Lesson2.hs
+++ Lesson2.hs
@@ -2,9 +2,25 @@ module Example
     (
     ) where
 
+import Data.Monoid
 
-reverse' :: String -> String
-reverse' [] = []
-reverse' (x : xs) = reverse' xs ++ [x]
+appendList :: [Int]
+appendList = [1] ++ [2]
 
--- reverse' "Gizra"
+appendList' :: [Int]
+appendList' = mappend [1] [2]
+
+appendList'' :: [Int]
+appendList'' = [1] `mappend` [2]
+
+
+-- How to append numbers?
+
+appendInt :: Int
+appendInt = mappend 5 6
+
+appendIntSum :: Sum Int
+appendIntSum = (Sum 5) `mappend` (Sum 6)
+
+appendIntSum' :: Sum Int
+appendIntSum' = 5 `mappend` 6
