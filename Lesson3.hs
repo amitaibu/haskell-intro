f61dac7cc196ab17106aad12da67ebdd0729c76f
Amitai Burstein <amitai@gizra.com>
2016-09-16 18:39:27 +0300

    fix

--- Lesson3.hs
+++ Lesson3.hs
@@ -7,5 +7,5 @@ data Clock12Hours = Clock12Hours Int
     deriving (Show)
 
 instance Monoid Clock12Hours where
-    mappend (Clock12Hours x) (Clock12Hours y) = Clock12Hours $ x + y `mod` 12
-    mempty = Clock12Hours 0
+    mappend (Clock12Hours x) (Clock12Hours y) = Clock12Hours $ (x + y) `mod` 12
+    mempty = Clock12Hours 12
